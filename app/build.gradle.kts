da177b98183aaa017279ab12201bf4672b7c553f
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-04 13:25:19 +0300

    add build.gradle.kts and settings.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("java")
+
+    id("application")
+    id("checkstyle")
+    id("jacoco")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.50.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("info.picocli:picocli:4.7.5")
+    implementation("io.javalin:javalin:6.0.0-beta.4")
+    implementation("org.slf4j:slf4j-simple:2.1.0-alpha1")
+    implementation("io.javalin:javalin-rendering:5.6.3")
+    implementation("gg.jte:jte:3.1.6")
+}
+
+tasks.withType<JavaExec> {
+    environment("PORT", "7000") // Установите порт по умолчанию
+}
