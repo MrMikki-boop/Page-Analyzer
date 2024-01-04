8437165c0abc789e4139ce97537ff93ac0a0341a
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-04 13:32:29 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,15 @@
 package hexlet.code;
-
 import io.javalin.Javalin;
-
 public class App {
     public static Javalin getApp() {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.result("Hello, World!"));
         return app;
     }
-
     public static void main(String[] args) {
-        getApp().start();
+        Javalin app = getApp();
+        app.start(7070);
     }
 }
