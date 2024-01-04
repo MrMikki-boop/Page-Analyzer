ded4ae8c9c7f2a0d2a023d8c4f5abcb63f71c153
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-04 15:04:10 +0300

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,7 +1,17 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
+    @Test
+    void testApp() {
+        Javalin app = App.getApp();
+
+        // Добавьте свои тесты здесь
+        assertNotNull(app);
+        // Например, можно добавить тесты для проверки конфигурации приложения или маршрутов
+    }
 }
