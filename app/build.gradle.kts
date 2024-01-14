15b9ab65de60f1a015209104f49c83bfd00f28c0
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-14 17:49:05 +0300

    Add build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.h2database:h2:2.2.224")
+
     implementation("info.picocli:picocli:4.7.5")
     implementation("io.javalin:javalin:6.0.0-beta.4")
     implementation("org.slf4j:slf4j-simple:2.1.0-alpha1")
