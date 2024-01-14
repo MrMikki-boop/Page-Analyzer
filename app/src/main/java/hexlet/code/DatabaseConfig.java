7491b54c9d6c711434e9e5a60ade49d51f00d8b3
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-14 17:59:58 +0300

    Refactor App and DatabaseConfig classes

--- app/src/main/java/hexlet/code/DatabaseConfig.java
+++ app/src/main/java/hexlet/code/DatabaseConfig.java
@@ -6,29 +6,29 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 
 public class DatabaseConfig {
-    private static final HikariConfig config = new HikariConfig();
+    private static final HikariConfig Config = new HikariConfig();
 
     static {
         // Задаем максимальное количество соединений в пуле
-        config.setMaximumPoolSize(10);
+        Config.setMaximumPoolSize(10);
         // Задаем таймаут на установку соединения с базой данных
-        config.setConnectionTimeout(5000);
+        Config.setConnectionTimeout(5000);
         // Задаем время ожидания неактивных соединений перед закрытием
-        config.setIdleTimeout(600000);
+        Config.setIdleTimeout(600000);
         // Задаем максимальное время жизни соединения в пуле
-        config.setMaxLifetime(1800000);
+        Config.setMaxLifetime(1800000);
     }
 
     public static HikariDataSource getDataSource() {
         if (System.getenv("JDBC_DATABASE_URL") != null) {
             // Если переменная окружения JDBC_DATABASE_URL установлена, используем ее значение (подходит для продакшена)
-            config.setJdbcUrl(System.getenv("JDBC_DATABASE_URL"));
+            Config.setJdbcUrl(System.getenv("JDBC_DATABASE_URL"));
         } else {
             // В противном случае (например, в локальной среде разработки) используем встроенную базу данных H2
-            config.setJdbcUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
-            config.setDriverClassName("org.h2.Driver");
+            Config.setJdbcUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+            Config.setDriverClassName("org.h2.Driver");
         }
-        return new HikariDataSource(config);
+        return new HikariDataSource(Config);
     }
 
     public static Connection getConnection() throws Exception {
