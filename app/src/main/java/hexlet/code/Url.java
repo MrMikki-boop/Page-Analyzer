831001aa0e393846994388788d67822199ab7285
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-17 05:41:51 +0300

    update Url.java

--- app/src/main/java/hexlet/code/Url.java
+++ app/src/main/java/hexlet/code/Url.java
@@ -7,5 +7,27 @@ public class Url {
     private String name;
     private Timestamp createdAt;
 
-    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
