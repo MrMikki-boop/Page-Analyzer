ab363a95e503ffb74fc4b230acf63e22c161e123
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-17 05:44:07 +0300

    add UrlRepository.java

---
+++ app/src/main/java/hexlet/code/UrlRepository.java
@@ -0,0 +1,20 @@
+package hexlet.code;
+
+import java.util.List;
+
+public class UrlRepository {
+
+    public void save(Url url) {
+        // реализация сохранения в базе данных
+    }
+
+    public List<Url> getAllUrls() {
+        // реализация получения всех URL из базы данных
+        return null;
+    }
+
+    public Url getUrlById(Long id) {
+        // реализация получения URL по id из базы данных
+        return null;
+    }
+}
