0a0c0f7fc627bf822f38c27e35bb75cc4b676281
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-19 01:34:44 +0300

    add UrlCheck.java and url.jte

---
+++ app/src/main/java/hexlet/code/UrlCheck.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.time.LocalDateTime;
+
+public class UrlCheck {
+    private Long id;
+    private Integer statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private Long urlId;
+    private LocalDateTime createdAt;
+
+    // геттеры и сеттеры
+
+    // конструкторы
+}
