6da806d0ff74e165f79cd00d3ff8e35deb11d8bd
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-01-19 10:59:49 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     application
+    jacoco
     id("java")
     id("checkstyle")
-    id("jacoco")
 
     id("io.freefair.lombok") version "8.4"
     id("com.adarshr.test-logger") version "4.0.0"
@@ -23,32 +23,24 @@ repositories {
 }
 
 dependencies {
-    // Тестирование
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.25.1")
-    testCompileOnly("org.projectlombok:lombok:1.18.30")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
-    testImplementation("com.squareup.okhttp3:mockwebserver:5.0.0-alpha.12")
-
-    // Логика приложения
-    implementation("info.picocli:picocli:4.7.5")
-    implementation ("org.slf4j:slf4j-api:1.7.32")
     implementation("io.javalin:javalin:5.6.3")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation("io.javalin:javalin-bundle:5.6.3")
-    implementation("io.javalin:javalin-rendering:5.6.3")
-    implementation("gg.jte:jte:3.1.6")
+    implementation("io.javalin:javalin-rendering:5.6.2")
+    implementation("gg.jte:jte:3.1.5")
     implementation("com.h2database:h2:2.2.224")
     implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("org.postgresql:postgresql:42.7.1")
-    implementation("com.konghq:unirest-java:4.0.0-RC2")
-    implementation("org.jsoup:jsoup:1.17.2")
-    implementation("org.postgresql:postgresql:42.2.5")
-    implementation ("ch.qos.logback:logback-classic:1.2.6")
-
+    implementation("org.postgresql:postgresql:42.7.0")
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
+    implementation("com.konghq:unirest-java:3.14.5")
+    implementation("org.jsoup:jsoup:1.17.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
 }
 
 tasks.withType<JavaExec> {
